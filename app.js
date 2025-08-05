adc9bd366906d61431b1fbc58ddca219d7b3f416
David <david16021@gmail.com>
2025-08-04 19:39:16 -0500

    Implementar funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+    // 1. Capturar el valor del input
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    // 2. Validar que no esté vacío
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return; // Salir de la función si está vacío
+    }
+
+    // 3. Agregar el nombre al array
+    amigos.push(nombreAmigo);
+    console.log(amigos)
+
+    // 4. Limpiar el input
+    document.getElementById("amigo").value = "";
+
+    // 5. Mostrar la lista actualizada en pantalla
+    actualizarLista();
+}
+
+// Función para mostrar los nombres en la lista <ul>
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar lista para no duplicar
+
+    amigos.forEach(function(amigo) {
+        let li = document.createElement("li"); // Crear <li>
+        li.textContent = amigo;
+        lista.appendChild(li); // Agregarlo a la <ul>
+    });
+}
